91588d04e230c26d3eaf830c7bac1e9c7e91e904
yasir <aff3ad@gmail.com>
2019-09-14 20:05:18 +0300

    Solution of courseinfo

--- part1/exercises/1/courseinfo/src/index.js
+++ part1/exercises/1/courseinfo/src/index.js
@@ -1,12 +1,67 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+/**
+ * Header Component
+ */
+    const Header = (props) => {
+        return(
+            <div>
+                <h1>{props.course}</h1>
+            </div>
+        )
+    }   
+
+ /**
+  * Content
+  */
+    const Content = (props) => {
+        return (
+            <div>
+              <Part part={props.courseName1} exercise={props.courseExercise1}/>
+              <Part part={props.courseName2} exercise={props.courseExercise2}/>
+              <Part part={props.courseName3} exercise={props.courseExercise3}/>
+            </div>
+        )
+    }
+  /**
+   * Total
+   */
+
+   const Total = (props) => {
+       return (
+           <div>
+               <p>Number of exercises {props.exercises}</p>
+           </div>
+       )
+   }
+
+   const Part = (props) => {
+       return (
+           <div>
+             <p>{props.part} {props.exercise}</p>
+           </div>
+       )
+   }
+
+  const App =  () => {
+    const course = 'Half Stack application development'
+    const part1 = 'Fundamentals of React'
+    const exercises1 = 10
+    const part2 = 'Using props to pass data'
+    const exercises2 = 7
+    const part3 = 'State of a component'
+    const exercises3 = 14
+      return(
+          <div>
+              <Header course={course} />
+              <Content 
+              courseName1={part1} courseExercise1={exercises1}
+              courseName2={part2} courseExercise2={exercises2}
+              courseName3={part3} courseExercise3={exercises3} />
+              <Total exercises= {exercises1+exercises2+exercises3} />
+          </div>
+      )
+  }
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
