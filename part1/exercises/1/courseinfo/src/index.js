28ea48aedde7bf068bb854b25d0091db96138b7b
yasir <aff3ad@gmail.com>
2019-09-15 18:37:13 +0300

    courseinfo solution 1.5 update

--- part1/exercises/1/courseinfo/src/index.js
+++ part1/exercises/1/courseinfo/src/index.js
@@ -43,13 +43,6 @@ import ReactDOM from 'react-dom';
    }
 
   const App =  () => {
-    /*const course = 'Half Stack application development'
-    const part1 = 'Fundamentals of React'
-    const exercises1 = 10
-    const part2 = 'Using props to pass data'
-    const exercises2 = 7
-    const part3 = 'State of a component'
-    const exercises3 = 14*/
     const course = {
         name: 'Half Stack application development',
         parts: [
